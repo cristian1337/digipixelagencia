d20f63cecd759d10ce39571da96a0e7cdabeb3ae
cristian <crispisjam@gmail.com>
2019-03-10 22:11:14 -0500

    agregue todos los archivos al repositorio

---
+++ js/app.js
@@ -0,0 +1,109 @@
+var contacto = document.getElementById('contactoForm');
+contactoForm.setAttribute('action','https://formspree.io/'+'crispisjam'+'@'+'gmail'+'.'+'com');
+
+$(document).ready(function(){
+    var controller = new ScrollMagic.Controller();
+    var escena = new ScrollMagic.Scene({
+        triggerElement: '#trigger ul',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#header-fade1', 'fade')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena2 = new ScrollMagic.Scene({
+        triggerElement: '#trigger2',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#header-fade2', 'fade')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena3 = new ScrollMagic.Scene({
+        triggerElement: '#trigger3',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#header-fade3', 'fade')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena4 = new ScrollMagic.Scene({
+        triggerElement: '#trigger4',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#mover', 'move')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena5 = new ScrollMagic.Scene({
+        triggerElement: '#trigger5',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#somos-fade1', 'fade')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena6 = new ScrollMagic.Scene({
+        triggerElement: '#trigger6',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#somos-fade2', 'fade')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena7 = new ScrollMagic.Scene({
+        triggerElement: '#trigger7',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#proyecto-move', 'desplazar')
+    // .addIndicators({
+    //     name: 'fade-scene',
+    //     colorTrigger: 'yellow',
+    //     colorStart: 'green'
+    // })
+    .addTo(controller);
+
+    var escena8 = new ScrollMagic.Scene({
+        triggerElement: '#trigger8',
+        triggerHook: 0,
+        reverse: false
+    })
+    .setClassToggle('#equipo-move', 'moveY')
+    // .addIndicators({
+    //     // name: 'fade-scene',
+    //     // colorTrigger: 'yellow',
+    //     // colorStart: 'green'
+    // })
+    .addTo(controller);
+});
